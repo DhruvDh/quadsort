f423d76c6b17240cc6f119a89de9ec8385784e00
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2020-02-16 20:16:33 -0500

    insertion

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
 #[cfg(test)]
+use rand;
+
+pub fn sort<T>(mut vec: Vec<T>) -> Vec<T>
+where
+    T: Ord,
+{
+    for j in 0..vec.len() {
+        let mut smallest = j;
+        for i in j..vec.len() {
+            if vec[i] < vec[smallest] {
+                smallest = i
+            }
+        }
+
+        vec.swap(smallest, j);
+    }
+
+    return vec;
+}
+
+#[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn sort_4() {
+        let vec: Vec<i32> = (0..2104).map(|_| rand::random()).collect();
+        let mut sorted = vec.clone();
+        
+        sorted.sort();
+
+        assert_eq!(super::sort(vec), sorted);
     }
 }
